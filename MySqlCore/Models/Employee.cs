8b266aed66972e591ac601b15dcfe490b7022802
saiko <saiko@DESKTOP-3LPIKD4>
2023-03-09 18:51:22 +0530

    Add project files.

---
+++ MySqlCore/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySqlCore.Models
+{
+    [Index(nameof(Email), IsUnique =true)]
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name field is required.")]
+        [StringLength(maximumLength:100, MinimumLength =2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email field is required.")]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
+        [EmailAddress]
+        public string  Email { get; set; }
+
+        [Required(ErrorMessage = "Phone field is required.")]
+        [StringLength(maximumLength: 15, MinimumLength = 10)]
+        public String Phone { get; set; }
+
+        [Required(ErrorMessage = "Address field is required.")]
+        [StringLength(maximumLength: 250)]
+        public string? Address { get; set; }
+    }
+}
